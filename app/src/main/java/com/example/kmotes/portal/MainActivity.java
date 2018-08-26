73d03195864e77c68c86197328d6726436626b41
kendall <kmotes.email@solutionreach.com>
2018-08-25 21:08:37 -0600

    gui initail commit

---
+++ app/src/main/java/com/example/kmotes/portal/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.kmotes.portal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        SwitchCompat onOffSwitch = (SwitchCompat)  findViewById(R.id.on_off_switch);
+        onOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.v("Switch State=", ""+isChecked);
+                if (isChecked)
+                {
+                    ((TextView)findViewById(R.id.status)).setText("Active");
+                }
+                else {
+                    ((TextView)findViewById(R.id.status)).setText("Off");
+                }
+            }
+
+        });
+    }
+}
