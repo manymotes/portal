ae53614dea0152f8fd30f4439b8cfcad3cc0d466
kendall <kmotes.email@solutionreach.com>
2018-08-27 23:19:23 -0600

    bluetooth stuff added

--- app/src/main/java/com/example/kmotes/portal/MainActivity.java
+++ app/src/main/java/com/example/kmotes/portal/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.kmotes.portal;
 
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.RemoteException;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SwitchCompat;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
-import android.widget.Switch;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class MainActivity extends AppCompatActivity implements BeaconConsumer {
+
+    private static final String TAG = "BEACON_PROJECT";
+    private ArrayList<String> beaconList;
+    private ListView beaconListView;
+    private ArrayAdapter<String> adapter;
+    private BeaconManager beaconManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,21 +42,86 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        SwitchCompat onOffSwitch = (SwitchCompat)  findViewById(R.id.on_off_switch);
+        SwitchCompat onOffSwitch = findViewById(R.id.on_off_switch);
         onOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 Log.v("Switch State=", ""+isChecked);
-                if (isChecked)
-                {
-                    ((TextView)findViewById(R.id.status)).setText("Active");
-                }
-                else {
-                    ((TextView)findViewById(R.id.status)).setText("Off");
-                }
+                if (isChecked) { ((TextView)findViewById(R.id.status)).setText("Active"); }
+                else { ((TextView)findViewById(R.id.status)).setText("Off"); }
             }
 
         });
+
+        //start beacon code
+
+        this.beaconList = new ArrayList<String>();
+        this.beaconListView = (ListView) findViewById(R.id.listView);
+        this.adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, this.beaconList);
+        this.beaconListView.setAdapter(adapter);
+        this.beaconManager = BeaconManager.getInstanceForApplication(this);
+        this.beaconManager.getBeaconParsers().add(new BeaconParser(). setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+        this.beaconManager.bind(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("This app needs location access");
+                builder.setMessage("Please grant location access so this app can detect beacons");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.M)
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+                    }
+                });
+                builder.show();
+            }
+        }
+
+
+        //end beacon code
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.beaconManager.unbind(this);
+    }
+
+    public void searchIbeacon() {
+
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        this.beaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                if (beacons.size() > 0) {
+                    beaconList.clear();
+                    for(Iterator<Beacon> iterator = beacons.iterator(); iterator.hasNext();) {
+                        beaconList.add(iterator.next().getId1().toString());
+                    }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            adapter.notifyDataSetChanged();
+                        }
+                    });
+                }
+            }
+        });
+        try {
+            this.beaconManager.startRangingBeaconsInRegion(new Region("MyRegionId", null, null, null));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
