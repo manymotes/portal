743ba4fd2ee0768940bf318f40965534a8b5526b
kendall <kmotes.email@solutionreach.com>
2018-08-28 23:42:03 -0600

    speed up the beacon scans to get more accurate distance readings.

--- app/src/main/java/com/example/kmotes/portal/MainActivity.java
+++ app/src/main/java/com/example/kmotes/portal/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
     private ListView beaconListView;
     private ArrayAdapter<String> adapter;
     private BeaconManager beaconManager;
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +64,11 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
         this.beaconManager = BeaconManager.getInstanceForApplication(this);
         this.beaconManager.getBeaconParsers().add(new BeaconParser(). setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
         this.beaconManager.bind(this);
+        beaconManager.setBackgroundScanPeriod(200);
+        beaconManager.setForegroundScanPeriod(200);
+        beaconManager.setBackgroundBetweenScanPeriod(200);
+        beaconManager.setForegroundBetweenScanPeriod(200);
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 final AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -105,9 +111,17 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
             @Override
             public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
                 if (beacons.size() > 0) {
-                    beaconList.clear();
+                    //beaconList.clear();
                     for(Iterator<Beacon> iterator = beacons.iterator(); iterator.hasNext();) {
-                        beaconList.add(iterator.next().getId1().toString());
+                        Beacon tempBeacon = iterator.next();
+                        if (tempBeacon.getId1().toString().equals("b9407f30-f5f8-466e-aff9-25556b57fd6e") || tempBeacon.getId1().toString().equals("b9407f30-f5f8-466e-aff9-25556b57fd6f")) {
+                            //beaconList.add(tempBeacon.getId1().toString());
+                            if (tempBeacon.getDistance() < 1.9411248985355725)
+                            {
+                                count += 1;
+                                beaconList.add(0, "OPEN: " + count);
+                            }
+                        }
                     }
                     runOnUiThread(new Runnable() {
                         @Override
@@ -125,3 +139,47 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
         }
     }
 }
+
+/*
+distnace notes
+
+3 inchase away is 0.0031161370689476646 mDistance
+
+10 ft away is 1.9411248985355725
+
+20 feet away is 2.0334158400692743
+
+id1: b9407f30-f5f8-466e-aff9-25556b57fd6e id2: 0 id3: 0
+beacon@5008
+
+id1: b9407f30-f5f8-466e-aff9-25556b57fd6f id2: 0 id3: 0
+beacon@5069
+mBluetoothAddress B8:27:EB:42:D6:B5
+
+ */
+
+
+
+
+
+//scan for ibeacon, get the major and minor vlaues
+
+// and major and minor to the uuid
+
+// let serviceUUID = [CBUUID(string: "D304DBD9-6FDC-4BF3-A617-E015" + majorValue + minorValue)]
+
+// look for characteristic id
+
+// peripheral.discoverCharacteristics([CBUUID.init(string :"5099CBC8-A71F-4292-8158-BF4F25AE9948"), CBUUID.init(string :"5099CBC8-A71F-4292-8158-BF4F25AE9949")], for: peripheral.services![0])
+
+//then turn on that characteristic
+
+//   peripheral.writeValue("GPIO_ON".data(using: .utf8)!, for: service.characteristics![0], type: .withoutResponse)
+
+// 5-10 feet open is optimal
+
+//look at power value to find distance
+
+//pi uses near setting for ibeacon to turn (far intermedieat and near)
+
+//phone will look
